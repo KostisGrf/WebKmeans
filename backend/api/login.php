2e3d4ac21cd034c8d645ccb735a7952d1a4e42b6
Kostis Grf <kostisgratsos@hotmail.gr>
2022-12-05 04:23:31 +0200

    ⚡️Updates login

--- backend/api/login.php
+++ backend/api/login.php
@@ -24,6 +24,8 @@ if(!isset($body['password'])){
     exit;
 }
 
+
+
 $email=$body['email'];
 $passwd=$body['password'];
 
@@ -36,19 +38,37 @@ $res = $res->fetch_assoc();
 
 
 if(!empty($res)){
+    
+    $sql='SELECT verified FROM users WHERE email=?';
+    $st2 = $mysqli->prepare($sql);
+    $st2->bind_param('s',$body['email']);
+    $st2->execute();
+    $res2 = $st2->get_result();
+    $r = $res2->fetch_all(MYSQLI_ASSOC);
+
+
+if(!$r[0]['verified']){
+    header("HTTP/1.1 400 Bad Request");
+	print json_encode(['errormesg'=>"You need to verify your email in order to login."]);
+    exit;
+}
     $check_password = password_verify($passwd, $res['password']);
+}else{
+        header("HTTP/1.1 400 Bad Request");
+	    print json_encode(['errormesg'=>"Wrong username/password."]);
+        exit;
 }
 
     
-    if($check_password){
-        $data=array("email"=>$res['email'],
-        "fname"=>$res["fname"],
-        "lname"=>$res["lname"],
-        "apiKey"=>$res["apiKey"]);
-        print json_encode($data, JSON_PRETTY_PRINT);
-    }else{
-        header("HTTP/1.1 400 Bad Request");
-	    print json_encode(['errormesg'=>"Wrong username/password."]);
-    }
+if($check_password){
+    $data=array("email"=>$res['email'],
+    "fname"=>$res["fname"],
+    "lname"=>$res["lname"],
+    "apiKey"=>$res["apiKey"]);
+    print json_encode($data, JSON_PRETTY_PRINT);
+}else{
+    header("HTTP/1.1 400 Bad Request");
+	print json_encode(['errormesg'=>"Wrong username/password."]);
+}
 
 ?>
