e9a179f6bd9a4fef33ea29e832dcc62eb27afcd1
Kostis Grf <kostisgratsos@hotmail.gr>
2022-12-06 02:43:31 +0200

    💄⚡️Creates forgot password feature

---
+++ backend/api/forgotPassword.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once '../dbconnect.php';
+
+$method=$_SERVER['REQUEST_METHOD'];
+$body = json_decode(file_get_contents("php://input"), true);
+
+if(!isset($body['email'])){
+    header("HTTP/1.1 400 Bad Request");
+    print json_encode(['errormesg'=>"email is required"]);
+    exit;
+}
+
+$token=bin2hex(random_bytes(16));
+
+$sql = 'call forgotPassword(?,?)';
+$st = $mysqli->prepare($sql);
+$st->bind_param('ss',$body['email'],$token);
+$st->execute();
+
+$sql2 = 'SELECT fname FROM users WHERE email=?';
+$st2 = $mysqli->prepare($sql2);
+$st2->bind_param('s',$body['email']);
+$st2->execute();
+$res = $st2->get_result();
+$res = $res->fetch_assoc();
+$fname=$res['fname'];
+
+$email_body="copy this to your browser $domain/backend/verify_delete.php?token=$token";
+$alt_body="copy this to your browser $domain/backend/verify_delete.php?token=$token";
+$subject="Password reset";
+send_mail($email,$fname,$subject,$email_body,$alt_body);
+print json_encode(['message'=>"check your email for link confirmation"]);
+
+
+?>
