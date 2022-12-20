72042f373f8aaf9fd03d4db8fbe77d7c3344bfc3
Kostis Grf <kostisgratsos@hotmail.gr>
2022-12-20 03:32:54 +0200

    ⚡️updates email verification

--- backend/verify_email.php
+++ backend/verify_email.php
@@ -2,6 +2,20 @@
 
 require_once 'dbconnect.php';
 
+
+if(!checkTokenExists($_GET['token'])){
+    header("HTTP/1.1 400 Bad Request");
+	print json_encode(['errormesg'=>"Token does not exist."]);
+	exit;
+}
+
+
+if(checkTokenExpired($_GET['token'])){
+    header("HTTP/1.1 401 Unauthorized ");
+    print json_encode(['errormesg'=>"token has expired"]);
+    exit;
+}
+
 $sql = 'call verifyAccount(?)';
 $st = $mysqli->prepare($sql);
 $st->bind_param('s',$_GET['token']);
