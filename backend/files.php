4c484b80eb371df82e500d2cc0a506bbca8f641c
Kostis Grf <kostisgratsos@hotmail.gr>
2022-12-20 03:26:16 +0200

    ✨Creates files functions

---
+++ backend/files.php
@@ -0,0 +1,10 @@
+<?php
+
+
+function rrmdir($directory)
+{
+    array_map(fn (string $file) => is_dir($file) ? rrmdir($file) : unlink($file), glob($directory . '/' . '*'));
+    return rmdir($directory);
+}
+
+?>
