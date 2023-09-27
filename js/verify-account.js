b727e2eb6be925458601a0a536134936a2522b14
Kostis Grf <kostisgratsos@hotmail.gr>
2023-09-28 02:45:52 +0300

    ⚡️minor updates on front end

--- js/verify-account.js
+++ js/verify-account.js
@@ -1,3 +1,10 @@
+
+ 
+    
+
+
+
+
 $(function(){
 
     function getUrlParams(k){
@@ -7,7 +14,6 @@ $(function(){
        }
 
     
-
     var token=getUrlParams('token');
     $.ajax({url: `./server/verify_email.php?token=${token}`,
     method: 'GET',
@@ -23,8 +29,72 @@ $(function(){
         <i class="fa-solid fa-triangle-exclamation fs-2 triangle"></i>
         <span class="fs-3">${errormesg.errormesg}</span>
     </div>`
+    let resend=`<hr>
+<div class="error-message error-resendemail">
+    <i class="fa-solid fa-triangle-exclamation"></i>
+    <span id="error-text-resendemail"></span>
+</div>
+<div id="resend-email-text"><a href="#" id="resend-link">Resend confirmation email</a>
+</div>
+<div class="text-center mt-3 text-primary" id="loading-spinner">
+    <div class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>`
     $('.card>.card-body').append(errorMessage);
+    $('.card>.card-body').append(resend);
       }
     })  
 
+    $(document).on('click', '#resend-link', function(){
+        $('.text-danger').hide();
+        $('#resend-email-text').hide();
+        $('#loading-spinner').show();
+        $('#token-text').hide();
+        
+
+         $.ajax({url: `./server/resend-email.php?token=${token}&type=regenerate-token`, 
+        method: 'GET',
+        dataType: 'json',
+        success:function(data){
+            $('#email-to').text(data['email'])
+            $('#email-dialog').show();
+            $('#loading-spinner').hide();
+        },
+        error:function(xhr, status, error) {
+            const response=JSON.parse(xhr.responseText);
+            $('#error-text-resendemail').text(response.errormesg);
+            $('.error-resendemail').show();
+            $('#resend-email-text').show();
+            $('#loading-spinner').hide();
+          }
+    })
+    })
+
+   
+
+    $('#resend-link2').click(()=>{
+        $('.error-resendemail2').hide();
+         let email=$('#email-to').text();
+        $('#resend-email-text2').hide();
+        $('#loading-spinner-small').show();
+         $.ajax({url: `./server/resend-email.php?email=${email}&type=email-verification`, 
+        method: 'GET',
+        success:function(){
+            $('#resend-email-text2').show();
+            $('#loading-spinner-small').hide();
+        },
+        error:function(xhr, status, error) {
+            const response=JSON.parse(xhr.responseText);
+            $('#error-text-resendemail2').text(response.errormesg);
+            $('.error-resendemail2').show();
+            $('#resend-email-text2').show();
+            $('#loading-spinner-small').hide();
+          }
+    })
+    }) 
+    
+    
+
 })
+
